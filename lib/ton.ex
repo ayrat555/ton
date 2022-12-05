120c6800a5e0849f2b3a71f00e56f7d9276efbe1
Ayrat Badykov <ayratin555@gmail.com>
2022-12-05 13:57:21 +0200

    mnemonic to entropy

--- lib/ton.ex
+++ lib/ton.ex
@@ -3,16 +3,31 @@ defmodule Ton do
   Documentation for `Ton`.
   """
 
+  # @pbkdf2_options %{alg: "sha512", iterations: 100_000, length: 32, format: true, salt: }
+
+  # @doc """
+  # Hello world.
+
+  # ## Examples
+
+  #     iex> Ton.hello()
+  #     :world
+
+  # """
+  # def mnemonic_to_seed(mnemonic) do
+  # end
+
   @doc """
-  Hello world.
+  Converts mnemonic to entropy.
 
   ## Examples
 
-      iex> Ton.hello()
-      :world
-
+      iex> Ton.mnemonic_to_entropy("rail sound peasant garment bounce trigger true abuse arctic gravity ribbon ocean absurd okay blue remove neck cash reflect sleep hen portion gossip arrow")
+      # "1ab77da4f35581bd6f8efdcaef689ec180c6f77d214e4dd960f1619f6c40599f04798cb4e93e30edceeb02ab36f450df1c359e7b3395f13646134046cc6a7f3a"
+      <<26, 183, 125, 164, 243, 85, 129, 189, 111, 142, 253, 202, 239, 104, 158, 193, 128, 198, 247, 125, 33, 78, 77, 217, 96, 241, 97, 159, 108, 64, 89, 159, 4, 121, 140, 180, 233, 62, 48, 237, 206, 235, 2, 171, 54, 244, 80, 223, 28, 53, 158, 123, 51, 149, 241, 54, 70, 19, 64, 70, 204, 106, 127, 58>>
   """
-  def hello do
-    :world
+  @spec mnemonic_to_entropy(String.t(), String.t()) :: binary()
+  def mnemonic_to_entropy(mnemonic, password \\ "") do
+    :crypto.mac(:hmac, :sha512, mnemonic, password)
   end
 end
--- test/ton_test.exs
+++ test/ton_test.exs
@@ -2,7 +2,31 @@ defmodule TonTest do
   use ExUnit.Case
   doctest Ton
 
-  test "greets the world" do
-    assert Ton.hello() == :world
+  describe "mnemonic_to_entropy/2" do
+    test "converts mnemonic to entropy" do
+      mnemonic =
+        "about about about about about about about about about about about about about about about about about about about about about about about about"
+
+      assert <<207, 159, 229, 215, 122, 194, 60, 170, 203, 21, 77, 169, 64, 28, 145, 253, 89, 172,
+               192, 117, 44, 37, 103, 174, 189, 137, 144, 113, 57, 96, 67, 66, 99, 217, 176, 3,
+               24, 3, 139, 173, 117, 211, 99, 101, 115, 199, 191, 252, 127, 197, 27, 203, 220,
+               217, 26, 35, 202, 189, 143, 94, 138, 185, 127,
+               179>> = entropy = Ton.mnemonic_to_entropy(mnemonic)
+
+      assert "cf9fe5d77ac23caacb154da9401c91fd59acc0752c2567aebd8990713960434263d9b00318038bad75d3636573c7bffc7fc51bcbdcd91a23cabd8f5e8ab97fb3" =
+               Base.encode16(entropy, case: :lower)
+    end
+
+    test "converts mnemonic with password to entropy" do
+      mnemonic =
+        "about about about about about about about about about about about about about about about about about about about about about about about about"
+
+      password = "password"
+
+      assert "893c3359695c384eafb038ba1a144917366b3e1a82c0f1c7755a5c0e9ec3967f53bbebb49b4c1bba4f350d08bc74fcca8c5aee3934d99f2c8d187ef09ccaee19" =
+               mnemonic
+               |> Ton.mnemonic_to_entropy(password)
+               |> Base.encode16(case: :lower)
+    end
   end
 end
