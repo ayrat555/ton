f7008f9b91a6d66785c9a15528db83afa91ff738
Ayrat Badykov <ayratin555@gmail.com>
2023-01-02 16:49:55 +0200

    fix external message for already deployed contract

--- test/ton/external_message_test.exs
+++ test/ton/external_message_test.exs
@@ -9,7 +9,7 @@ defmodule ExternalMessageTest do
   alias Ton.Wallet
 
   describe "serialize/2" do
-    test "serializes an external message" do
+    test "serializes an external message (already deployed contract)" do
       keypair =
         Ton.mnemonic_to_keypair(
           "about about about about about about about about about about about about about about about about about about about about about about about about"
@@ -24,17 +24,49 @@ defmodule ExternalMessageTest do
           value: 1,
           bounce: false,
           to: address,
-          wallet_id: wallet.wallet_id
+          wallet_id: wallet.wallet_id,
+          epoch_timeout: 1_672_315_426
         )
         |> Transfer.serialize_and_sign(keypair.secret_key)
 
-      common_message_info = CommonMessageInfo.new(wallet, transfer)
+      common_message_info = CommonMessageInfo.new(nil, transfer)
 
-      address
-      |> ExternalMessage.new(common_message_info)
-      |> ExternalMessage.serialize()
-      |> Cell.hash()
-      |> IO.inspect()
+      assert "74cf905a7d5d7e66c11efbcf8128ced5abfb14a7405c6e9be097fc7af3ec3528" ==
+               wallet
+               |> ExternalMessage.new(common_message_info)
+               |> ExternalMessage.serialize()
+               |> Cell.hash()
+               |> Base.encode16(case: :lower)
     end
+
+    # test "serializes an external message (a new contract)" do
+    #   keypair =
+    #     Ton.mnemonic_to_keypair(
+    #       "house about about about about about about about about about about about about about about about about about about about about about about about"
+    #     )
+
+    #   wallet = Wallet.create(0, keypair.public_key)
+    #   {:ok, address} = Address.parse("EQAHJQ6gs2NYAXsxsfsucpqhpneZaGP0qCdu9lCEzysMGzst")
+
+    #   transfer =
+    #     Transfer.new(
+    #       seqno: 0,
+    #       value: 1,
+    #       bounce: false,
+    #       to: address,
+    #       wallet_id: wallet.wallet_id,
+    #       epoch_timeout: 1_672_315_426
+    #     )
+    #     |> Transfer.serialize_and_sign(keypair.secret_key)
+
+    #   common_message_info = CommonMessageInfo.new(wallet, transfer)
+
+    #   assert "74cf905a7d5d7e66c11efbcf8128ced5abfb14a7405c6e9be097fc7af3ec3528" ==
+    #            wallet
+    #            |> ExternalMessage.new(common_message_info)
+    #            |> ExternalMessage.serialize()
+    #            |> Cell.hash()
+    #            |> Base.encode16(case: :lower)
+    # end
   end
 end
