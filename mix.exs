6e7e1046a501864d921124b6793af985d047d661
Ayrat Badykov <ayratin555@gmail.com>
2023-03-23 17:45:55 +0200

    use precompiled evil_crc32c

--- mix.exs
+++ mix.exs
@@ -42,8 +42,7 @@ defmodule Ton.MixProject do
       {:ex_pbkdf2, "~> 0.7.1"},
       {:cafezinho, "~> 0.3.1"},
       {:mnemoniac, "~> 0.1.0"},
-      {:evil_crc32c, "~> 0.1.2"},
-      {:rustler, "~> 0.26"},
+      {:evil_crc32c, "~> 0.2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
